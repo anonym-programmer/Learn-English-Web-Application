05f5693332df597afd8d68ef0c14e054aee04be7
RobertK <robert.krzywina@gmail.com>
2019-03-14 16:37:37 +0100

    Initial commit

---
+++ server/src/main/java/pl/robert/api/SpringBootAPI.java
@@ -0,0 +1,12 @@
+package pl.robert.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAPI {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAPI.class, args);
+    }
+}
